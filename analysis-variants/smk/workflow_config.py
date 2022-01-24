12c2fbf7b76d912871c62dfeec7876dc174b0de5
Lachlan Baer <baerlachlan@gmail.com>
2022-01-24 10:36:28 +1030

    Small changes

--- analysis-variants/smk/workflow_config.py
+++ analysis-variants/smk/workflow_config.py
@@ -162,8 +162,8 @@ class RNASeqVariantDiscovery:
                 "dna.primary_assembly.fa.gz"
             ])
         )
-        self.refFa = self.refGz.rstrip(".gz")
         self.refGz_path = os.path.join(self.refs_dir, self.refGz)
+        self.refFa = self.refGz.rstrip(".gz")
         self.refFa_path = os.path.join(self.refs_dir, self.refFa)
         self.ref_url = os.path.join(
             "rsync://ftp.ensembl.org/ensembl/pub",
